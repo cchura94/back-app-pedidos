3d0d40af937875ea4244d63b67b48cc05ca37576
cchura94 <cchura.cpc@gmail.com>
2020-09-17 21:33:15 -0400

    lista, mostrar, guardar de Producto

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -1,13 +1,48 @@
-const listar = function (req, res) {
-  res.json({ mensaje: "Lista de Productos" });
+const Producto = require("./../models/Producto");
+const { isValidObjectId } = require("mongoose");
+
+const listar = async function (req, res) {
+  /*Producto.find()
+    .then((datos) => {
+      res.json(datos);
+    })
+    .catch((error) => {
+      console.log(error);
+    });*/
+  try {
+    const datos = await Producto.find();
+    res.json(datos);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const mostrar = function (req, res) {
-  res.json({ mensaje: "Mostrar Producto" });
+const mostrar = async function (req, res) {
+  try {
+    const dato = isValidObjectId(req.params.id);
+    if (dato) {
+      const producto = await Producto.findById(req.params.id);
+      res.json({ mensaje: "producto", datos: producto });
+    } else {
+      res.json({ mensaje: "Producto no encontrado", status: 404 });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      mensaje: "Ocurrio un error al buscar el producto",
+      status: 404,
+    });
+  }
 };
 
 const guardar = function (req, res) {
-  res.json({ mensaje: "Guardar Producto" });
+  try {
+    const prod = new Producto(req.body);
+    prod.save();
+    res.json({ mensaje: "Producto registrado", prod });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const modificar = function (req, res) {
