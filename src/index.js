be24c730393b8617cdf41191f65ca3543041534a
cchura94 <cchura.cpc@gmail.com>
2020-10-03 23:40:22 -0400

    CORS

--- src/index.js
+++ src/index.js
@@ -30,6 +30,21 @@ mongoose
 //app.set("nombre", "Cristian");
 //console.log(app.get("nombre"));
 
+//Habilitando CORS para peticiones del lado del cliente
+app.use(function (req, res, next) {
+  //Sitio web al que se desea permitir que se conecte
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  //habilitar los encabezados que desea permitir
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "content-type, X-Requested-With, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+  next();
+});
+
 //Habilitar rutas
 rutas(app);
 
@@ -37,43 +52,3 @@ rutas(app);
 app.listen(app.get("port"), () => {
   console.log(`Servidor corriendo en: 127.0.0.1:${app.get("port")}`);
 });
-
-/*
-
-
-//IMPORTS
-import express from "express";
-import morgan from "morgan";
-import mongoose from "mongoose";
-import port from "./config/config";
-//importando el modulo de rutas
-import { rutas } from "./routes/index.js";
-import { port } from "./config/config";
-
-//CONFIGS
-const app = express();
-app.use(morgan("dev"));
-app.set("port", process.env.PORT || port);
-// levantar servidor
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for pa
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencodedapp.set("port", process.env.PORT || _port);
-//CONFIG DB
-mongoose
-  .connect("mongodb://localhost:27017/app-pedido", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((db) => {
-    console.log("DB Conectado...");
-  })
-  .catch((error) => {
-    console.log("Error de conexion:", error);
-  });
-//HABILITAR RUTAS
-rutas(app);
-//LEVANTAR SERVICIO
-app.listen(app.get("port"), () => {
-  co
-
-  */
